22d06ffe9739b93f432e2ae7722165b62d3a7411
ShivaniGupta164 <166619882+ShivaniGupta164@users.noreply.github.com>
2025-04-10 17:31:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white text-3xl font-bold">
+      Tailwind CSS is working! 🚀
+    </div>
+  );
+}
+
+export default App;
